7ab8873bc24261f7daecbb3fe6b5fcd0959778c4
Mihran Maruqyan <mihran_maruqyan@epam.com>
2021-09-02 21:37:09 +0400

    Initial commit

---
+++ src/main/java/com/example/grpc_redis/GrpcRedisApplication.java
@@ -0,0 +1,13 @@
+package com.example.grpc_redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrpcRedisApplication.class, args);
+	}
+
+}
